8c17c185d2f8701241271efde6b076b4a302b897
zhuxu <zhuxu107@gmail.com>
2013-11-30 21:25:09 +0800

    testing file execution

---
+++ Chapter11/test17.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# testing file execution
+if [ -x test16.sh ]
+then
+	echo "You can run the script:"
+	./test16.sh
+else
+	echo "Sorry, you are not able to execute the script"
+fi
+
