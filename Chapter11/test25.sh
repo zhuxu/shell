fe6243eb2fba0aa48c2a278930b5244ffcc847ea
zhuxu <zhuxu107@gmail.com>
2013-12-02 22:50:59 +0800

    looking for a possable value

---
+++ Chapter11/test25.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# looking for a possible value
+
+if [ $USER = "rich" ]
+then
+	echo "Welcome $USER"
+	echo "Please enjoy your visit"
+elif [ $USER = barbara ]
+then
+	echo "Welcome $USER"
+	echo "Please enjoy your visit"
+elif [ $USER = testing ]
+then
+	echo "Special testing account"
+elif [ $USER = jessica ]
+then
+	echo "Do not forget to logout when you're done"
+else
+	echo "Sorry, you are not allowed here"
+fi
