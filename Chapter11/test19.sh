028eb05bfaf9e6aeab3043b57aa2c9f8d9f6aac4
zhuxu <zhuxu107@gmail.com>
2013-12-17 16:50:04 +0800

    check file group test

--- Chapter11/test19.sh
+++ Chapter11/test19.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 # check file group test
+
+if [ -G $HOME/shell/Chapter11/test19.sh ]
+then
+	echo "You are in the same group as the file"
+else
+	echo "The file is not owned by your group"
+fi
