0884b5ea255ff8add1a12fee57c65d8dba0d1589
zhuxu <zhuxu107@gmail.com>
2014-01-18 21:30:39 +0800

    testing closing file descriptor

---
+++ Chapter14/test17.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# testing closing file descriptors
+
+exec 3> test17file
+echo "This is a test line of data" >&3
+exec 3>&-
+
+cat test17file
+exec 3>test17file
+echo "This'll be bad" >&3
